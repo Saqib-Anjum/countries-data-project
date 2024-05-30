569ab53a7a0a66c7452a2cd9aa6ff0af60ce4f18
miketysonkeil <miketyson.keil@gmail.com>
2024-05-31 00:41:53 +0500

    countries Project

---
+++ components/CountriesListShimmer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './CountriesListShimmer.css'
+
+
+
+export default function CountriesListShimmer() {
+    // new Array(10).fill('')
+  
+    return (
+      <div className="countries-container">
+        {Array.from({ length: 12 }).map((el, i) => {
+          return (
+            <div key={i} className="country-card shimmer-card">
+              <div className="flag-container"></div>
+              <div className="card-text">
+                <h3 className="card-title"></h3>
+                <p></p>
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+/* export default function CountriesListShimmer() {
+    const mapped = Array.from({length: 12}).map((el, i) => {
+        return <div key={i} className="country-card shimmer-card"></div>
+
+    })
+  return (
+
+    <div className="countries-container">
+        {
+            mapped
+        }
+
+    </div>
+  )
+} */
